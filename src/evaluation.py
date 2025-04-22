d86155687a1a54d115dc471ec1048265d0d72a57
Hackteam Heidelberg <hallo@stadtlabor.network>
2025-04-22 20:00:15 +0200

    new features

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,9 +2,11 @@ import torch
 import json
 import tomllib
 import deepeval
+import argparse
 from deepeval import evaluate
 from deepeval.metrics import FaithfulnessMetric
-from deepeval.test_case import LLMTestCase
+from deepeval.test_case.llm_test_case import LLMTestCase
+from deepeval.models.llms.ollama_model import OllamaModel
 from deepeval.models.base_model import DeepEvalBaseLLM
 import transformers
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -15,7 +17,7 @@ from lmformatenforcer.integrations.transformers import (
     build_transformers_prefix_allowed_tokens_fn,
 )
 
-from query import RAG_LLM, huggingface_login
+import query
 
 
 class EvalHuggingFaceLLM(DeepEvalBaseLLM):
@@ -76,30 +78,59 @@ class EvalHuggingFaceLLM(DeepEvalBaseLLM):
     def get_model_name(self):
         return "Llama-3.1 8B"
 
-rag_llm = RAG_LLM()
-eval_llm = EvalHuggingFaceLLM()
 
-questions = ["Wie viele Unterlagen des Finanzausschusses sind vorhanden und welche sind das?"]
-scores = []
-
-metric_faithful = FaithfulnessMetric(model=eval_llm)
-
-for question in questions:
-    retrieved_texts = rag_llm.search_relevant_documents(user_query=question)[1]
-    generated_answer = rag_llm.query_rag_llm(user_query=question)
-
-    print(f"Question: {question}")
-    print(f"Retrieved text: {retrieved_texts}")
-    print(f"Answer: {generated_answer}")
-
-    test_case = LLMTestCase(
-        input=question,
-        actual_output=generated_answer,
-        retrieval_context=retrieved_texts
+def evaluate_model(config: str, secrets: str) -> None:
+    
+    eval_llm = OllamaModel( # EvalHuggingFaceLLM()
+        model="llama3.2",
+        base_url="http://localhost:11434"
     )
+    questions = ["Wie viele Unterlagen des Finanzausschusses sind vorhanden und welche sind das?"]
+    scores = []
 
-    metric_faithful.measure(test_case)
-    print(metric_faithful.score)
-    print(metric_faithful.reason)
+    metric_faithful = FaithfulnessMetric(model=eval_llm)
 
-    scores.append((metric_faithful.score, question))
+    for question in questions:
+        retrieved_texts = query.retrieve(config=config, secrets=secrets, user_query=question)
+        generated_answer = query.query(config=config, secrets=secrets, user_query=question)
+
+        print(f"Question: {question}")
+        print(f"Retrieved text: {retrieved_texts}")
+        print(f"Answer: {generated_answer}")
+
+        test_case = LLMTestCase(
+            input=question,
+            actual_output=generated_answer,
+            retrieval_context=retrieved_texts
+        )
+
+        metric_faithful.measure(test_case)
+        print(metric_faithful.score)
+        print(metric_faithful.reason)
+
+        scores.append((metric_faithful.score, question))
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', '-c', type=str, default=None, help='path to configfile')
+    parser.add_argument('--secrets', '-s', type=str, default=None, help='path to secretsfile')
+    parser.add_argument('--verbose', '-v', action='store_true', help='be verbose')
+    parser.add_argument('--framework', '-f', type=str, choices=query.FRAMEWORKS, default=None, help='framework haystack or llamastack')
+    parser.add_argument('--retriever', '-r', action='store_true', help='use only retriever')
+    
+    args = parser.parse_args()
+    if args.secrets:
+        secrets = query.read_config(args.secrets)
+    else:
+        secrets = query.read_config(query.DEFAULT_SECRETSFILE)
+    if args.config:
+        config = query.read_config(args.config)
+    else:
+        config = query.read_config(query.DEFAULT_CONFIGFILE)
+    #set the verbose flag in config intead of passing around as parameter
+    #use utils.py function vprint to print only if verbose is set
+    if args.verbose:
+        config['verbose'] = 1 #This allows to set verbosity levels later
+
+    evaluate_model(config, secrets)
